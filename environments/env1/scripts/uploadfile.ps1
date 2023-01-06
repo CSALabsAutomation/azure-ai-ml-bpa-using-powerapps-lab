0da03c87bc71a15e8987834f3240078e08cc8937
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2023-01-06 14:29:20 +0530

    Update uploadfile.ps1

--- environments/env1/scripts/uploadfile.ps1
+++ environments/env1/scripts/uploadfile.ps1
@@ -8,6 +8,8 @@ Write-Host "file upload script started"
        Install-Module -Name Az.Accounts -Force
     }
 
+#############################
+
 $uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/BPA_Template_v2_Current.zip";
 $bacpacFileName = "BPA_Template_v2_Current.zip";
 
@@ -20,5 +22,74 @@ Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'BP
 
 #############################
 
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/BPA_Template_v2_Current.zip";
+$bacpacFileName = "BPA_Template_v2_Current.zip";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'BPA_Template_v2_Current.zip' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/BPA_Template_v2_Current.zip";
+$bacpacFileName = "BPA_Template_v2_Current.zip";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'BPA_Template_v2_Current.zip' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/BPA_Template_v2_Current.zip";
+$bacpacFileName = "BPA_Template_v2_Current.zip";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'BPA_Template_v2_Current.zip' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/BPA_Template_v2_Current.zip";
+$bacpacFileName = "BPA_Template_v2_Current.zip";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'BPA_Template_v2_Current.zip' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/BPA_Template_v2_Current.zip";
+$bacpacFileName = "BPA_Template_v2_Current.zip";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'BPA_Template_v2_Current.zip' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/BPA_Template_v2_Current.zip";
+$bacpacFileName = "BPA_Template_v2_Current.zip";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'BPA_Template_v2_Current.zip' -Context $ctx
 
 Write-Host "file upload script completed"
