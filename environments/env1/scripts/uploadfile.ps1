a99a70a5983281d0eb3c8688eb9ef604f0daff48
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2023-01-09 21:24:27 +0530

    Update uploadfile.ps1

--- environments/env1/scripts/uploadfile.ps1
+++ environments/env1/scripts/uploadfile.ps1
@@ -30,7 +30,7 @@ $storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupNa
 $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
 
 Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
-Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'invoice1.pdf' -Context $ctx
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TrainingDocuments/invoice1.pdf' -Context $ctx
 
 #############################
 
@@ -42,7 +42,7 @@ $storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupNa
 $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
 
 Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
-Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'invoice2.pdf' -Context $ctx
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TrainingDocuments/invoice2.pdf' -Context $ctx
 
 #############################
 
@@ -54,7 +54,7 @@ $storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupNa
 $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
 
 Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
-Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'invoice3.pdf' -Context $ctx
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TrainingDocuments/invoice3.pdf' -Context $ctx
 
 #############################
 
@@ -66,7 +66,7 @@ $storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupNa
 $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
 
 Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
-Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'invoice4.pdf' -Context $ctx
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TrainingDocuments/invoice4.pdf' -Context $ctx
 
 #############################
 
@@ -78,4 +78,52 @@ $storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupNa
 $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
 
 Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
-Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'invoice5.pdf' -Context $ctx
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TrainingDocuments/invoice5.pdf' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/TestDocuments/invoice1.pdf";
+$bacpacFileName = "invoice1.pdf";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TestDocuments/invoice1.pdf' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/TestDocuments/invoice2.pdf";
+$bacpacFileName = "invoice2.pdf";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TestDocuments/invoice2.pdf' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/TestDocuments/invoice3.pdf";
+$bacpacFileName = "invoice3.pdf";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TestDocuments/invoice3.pdf' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-ai-ml-bpa-using-powerapps-lab/main/environments/env1/Artifacts/TestDocuments/invoice4.pdf";
+$bacpacFileName = "invoice4.pdf";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "bpa-assets" -Blob 'TestDocuments/invoice4.pdf' -Context $ctx
